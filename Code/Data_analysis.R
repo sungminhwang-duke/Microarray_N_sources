42f4aeffab6852f826be3e44320d3745ae779595
sungminhwang-duke <45494621+sungminhwang-duke@users.noreply.github.com>
2019-05-16 12:53:42 -0400

    Update Data_analysis.R

--- Code/Data_analysis.R
+++ Code/Data_analysis.R
@@ -5,7 +5,7 @@ library(dplyr)
 library(tidyr)
 
 # Input the normalized data
-microarray_data <- read.csv("H26_all_genes_normalized.csv")
+microarray_data <- read.csv(file.choose("H26_all_genes_normalized.csv"))
 
 # Identify the correlation coefficient and the significance level(or p-value) from the two different N-sources (alanine vs ammonium chloride) with each biological replicates
 
@@ -49,41 +49,58 @@ grid.arrange(plot4, plot5, plot6, ncol=3)
 
 
 # Plot a volcano plot
-H26_DE <- read.csv("#Input .csv format")
+H26_DE <- read.csv(file.choose("H26_all_genes_normalized.csv"))
 head(H26_DE)
 with(H26_DE, plot(log2FoldChange, -log10(pvalue), pch=20, main="Volcano plot"))
 
-# Add color points as following categories: green if qvalue<0.05, orange if log2FC>1, red if both)
-with(subset(H26_DE, qvalue<0.05 ), points(log2FoldChange, -log10(pvalue), pch=20, col="green"))
-with(subset(H26_DE, abs(log2FoldChange)>1), points(log2FoldChange, -log10(pvalue), pch=20, col="orange"))
+# Add color points as following categories: red if both qvalue<0.05 and log2FC>1)
 with(subset(H26_DE, qvalue<0.05 & abs(log2FoldChange)>1), points(log2FoldChange, -log10(pvalue), pch=20, col="red"))
 
 # Identify the significant genes in the changed growth condition
 print(subset(H26_DE, qvalue<0.05 & abs(log2FoldChange)>1))
 arrangement <- print(subset(H26_DE, qvalue<0.05 & abs(log2FoldChange)>1))
 print_arrangement <- print(arrangement[,2])
+write.csv(arrangement, "Sig_genes.csv")
 
-# Label the significant genes from the calibrate plot
+# Label the significant genes from the calibrate plot, if needed.
 library(calibrate)
+
 with(subset(H26_DE, qvalue<0.05 & abs(log2FoldChange)>1), textxy(log2FoldChange, -log10(pvalue), labs=Gene, cex=.4))
 
-# Cluster the significant genes and visualized by Heatmap
-library("gplots")
-library("heatmap.plus")
-library("RColorBrewer")
+# Cluster the significant genes and visualized by Heatmap (hierarchical clusterings with dendextend)
+library(tidyr)
+library(dplyr)
+library(dendextend)
+library(RColorBrewer)
+library(gplots)
 
-test <- read.csv("#Input .csv format", row.names = 1)
-NH4Cl <- test[grep("NH4",row.names(test)),]
-Alanine <- test[grep("alanine",row.names(test)),]
+sig.gene <- read.csv(file.choose("Significant_genes.csv"))
+sig.gene.long <- gather(sig.gene, gene, expression, -condition)
 
-# http://www.rapidtables.com/web/color/RGB_Color.htm
-condition_colors <- unlist(lapply(rownames(test),function(x){
-  if(grepl("alanine",x)) '#FFC0CB' #pink
-  else if(grepl('NH4',x)) '#808080' #grey
-}))
+sig.gene.cor <- select(sig.gene, -condition) %>% 
+  cor(use="pairwise.complete.obs", method = "pearson")
+sig.gene.dist <- as.dist(1 - sig.gene.cor)
 
-input <- as.matrix(t(test))
-heatmap.2(input, hclust=function(x) hclust(x,method="average"), trace="none", density="none", col=bluered(20), cexRow=0.5, cexCol=0.3, margins = c(5,10),
-          ColSideColors=condition_colors, scale="row", main="Less than 5% FDR")
-legend(0.75, 0.9,legend=c("NH4Cl","Alanine"),fill=c('#808080','#FFC0CB'),cex=0.3)
+sig.gene.tree <- hclust(sig.gene.dist, method="complete")
+plot(sig.gene.tree)
+sig.gene.dend <- as.dendrogram(sig.gene.tree)
 
+plot(sig.gene.dend, leaflab = "none")
+sig.clusters <- cutree(sig.gene.dend, k=3)
+
+data.mtx <- as.matrix(select(sig.gene, -condition))
+row.names(data.mtx) <- sig.gene$condition
+transposed.data.mtx <- t(data.mtx)
+
+color.scheme <- rev(brewer.pal(8,"RdYlBu"))
+
+heatmap.2(transposed.data.mtx,
+          Rowv = sig.gene.dend,  
+          Colv = NULL,
+          dendrogram = "row", 
+          col = color.scheme,
+          trace = "none", density.info = "none",
+          scale="row",
+          cexRow=0.7, cexCol=0.5, 
+          margins = c(5,5),
+          xlab = "Growth condition")
